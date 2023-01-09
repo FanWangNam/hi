4a58f0f29e13515db3cb61e0be2786e0383deda7
FanWangNam <fanfionawang@gmail.com>
2023-01-09 15:32:34 -0800

    Commit Message:name, username, date

---
+++ animal.py
@@ -0,0 +1,8 @@
+# Author: Fan Wang
+# GitHub username: FanWangNam
+# Date: 1/9/2023
+# Description: Asks the user for their favorite animal and then
+#              prints out that that is their favorite animal
+
+fave_animal = input("Please enter your favorite animal: ")
+print("Your favorite animal is the " + fave_animal + ".")
