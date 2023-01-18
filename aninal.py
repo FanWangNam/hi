610f01f3f06b565722175101319bdf12be081291
FanWangNam <110063409+FanWangNam@users.noreply.github.com>
2023-01-17 19:53:26 -0800

    Delete aninal.py

--- aninal.py
+++
@@ -1,8 +0,0 @@
-# Author: Fan Wang
-# GitHub username: FanWangNam
-# Date: 1/9/2023
-# Description: Asks the user for their favorite animal and then
-#              prints out that that is their favorite animal
-
-fave_animal = input("Please enter your favorite animal: ")
-print("Your favorite animal is the " + fave_animal + ".")
