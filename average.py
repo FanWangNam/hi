5642f2afbaa7549e2d1c91ba3109a38a9086bef2
FanWangNam <fanfionawang@gmail.com>
2023-01-17 19:44:43 -0800

    Commit Message:name, username, date

---
+++ average.py
@@ -0,0 +1,16 @@
+number_1 = float(input("Please enter first number: "))
+number_2 = float(input("Please enter second number: "))
+number_3 = float(input("Please enter third number: "))
+number_4 = float(input("Please enter fourth number: "))
+number_5 = float(input("Please enter fifth number: "))
+
+print("Please enter five numbers.")
+print(number_1)
+print(number_2)
+print(number_3)
+print(number_4)
+print(number_5)
+
+print("The average of those numbers is: ")
+average_num = (number_1 + number_2 + number_3 + number_4 + number_5) / 5
+print(average_num)
