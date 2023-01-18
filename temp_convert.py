2b1a76c308a455713f776911d6e9376eb07a87d3
FanWangNam <fanfionawang@gmail.com>
2023-01-17 20:33:05 -0800

    Commit Message:name, username, date

---
+++ temp_convert.py
@@ -0,0 +1,6 @@
+c_temperature = float(input())
+print("Please enter a Celsius temperature.")
+print(c_temperature)
+f_temperature = (9/5) * c_temperature + 32
+print("The equivalent Fahrenheit temperature is:")
+print(f_temperature)
