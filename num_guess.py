310999e8af2e260db2c1f921402cf001f0286930
FanWangNam <fanfionawang@gmail.com>
2023-01-22 21:52:59 -0800

    Commit Message:name, username, date

---
+++ num_guess.py
@@ -0,0 +1,21 @@
+number = int(input("Enter the integer for the player to guess."))
+print(number)
+
+guess_number = ""
+guess_count = 0
+while guess_number != number:
+    guess_count += 1
+    guess_number = int(input("Enter your guess."))
+    print(guess_number)
+
+    if guess_number > number:
+        print("Too high - try again: ")
+    elif guess_number < number:
+        print("Too low - try again: ")
+    else:
+        print(f"You guessed it in {guess_count} tries")
+
+
+
+
+
