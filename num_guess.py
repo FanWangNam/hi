2fe88daa8bfd4f2249c0d1af65dde314d0fe3bd2
FanWangNam <fanfionawang@gmail.com>
2023-01-23 17:36:58 -0800

    Commit Message:name, username, date

--- num_guess.py
+++ num_guess.py
@@ -12,10 +12,10 @@ while guess_number != number:
         print("Too high - try again:")
     elif guess_number < number:
         print("Too low - try again:")
+    elif guess_number == number and guess_count == 1:
+        print(f"You guessed it in {guess_count} try.")
     else:
-        print(f"You guessed it in {guess_count} tries.")
-
-
+        print(f"You guessed it in {guess_count} try.")
 
 
 
